d2a316be6a534c383cf1a7de24c9e39250a6bffa
wondrousWebWorks <alberts.johann@gmail.com>
2019-12-18 16:13:15 +0000

    Added jQuery to call modal on Information Image click.

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $( "#audio-controls-toggle" ).click(function() {
         $( "#audio-controls" ).toggle();
     });
+
+    $( "#info-icon" ).click(function() {
+        $( "#info-modal" ).modal();
+    })
 });
