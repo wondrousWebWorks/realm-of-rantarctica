ccba4feca09c579ce4847edb3f2734df7c6c682c
wondrousWebWorks <alberts.johann@gmail.com>
2019-12-19 15:34:14 +0000

    Added html code for the Landing Page as a string literal to be loaded by jQuery.

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,1 +1,17 @@
 /* The html code for the Landing Page stored as a string literal and to be loaded by jQuery */
+let landingPage = `
+    <div id="game-title-and-icons-container">
+        <h1 id="game-title">RealM of RantarcticA</h1>
+        <div id="landing-page-icons">
+            <img id="play-icon" class="landing-page-icon"
+                src="https://res.cloudinary.com/wondrouswebworks/image/upload/v1576668266/realm-of-rantarctica/icons/Play_mixgfj.png"
+                alt="play button">
+            <img id="info-icon" class="landing-page-icon"
+                src="https://res.cloudinary.com/wondrouswebworks/image/upload/v1576668266/realm-of-rantarctica/icons/Info_kcqddf.png"
+                alt="play button">
+        </div>
+        <a id="wondrouswebworks-logo" href="https://github.com/wondrousWebWorks" target="_blank"><img
+          <a id="wondrouswebworks-logo" href="https://github.com/wondrousWebWorks" target="_blank"><img
+            src="https://res.cloudinary.com/wondrouswebworks/image/upload/v1570699539/mp1-cape-safari/wondrous-large_rrqmc8.png"
+            alt="wondrousWebWorks logo"></a>        
+    </div>`;
