a4a7110803c552dc416caa251593f731e3dfcbe3
wondrousWebWorks <alberts.johann@gmail.com>
2019-12-19 15:18:01 +0000

    Added code for loading the newly created landingPage html code as a string literal.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
         $( "#audio-controls" ).toggle();
     });
 
+    $( "#full-screen-game" ).html(landingPage);
+
     $( "#info-icon" ).click(function() {
         $( "#info-modal" ).modal();
-    })
+    });
+
 });
