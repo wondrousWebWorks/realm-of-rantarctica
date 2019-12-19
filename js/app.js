d4b9a1db0f5bb0f6872b990271ef9d3b569a912b
wondrousWebWorks <alberts.johann@gmail.com>
2019-12-19 15:34:59 +0000

    Added comments to all jQuery commands so far.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+    /* Toggles the html audio control's visibility on clicking */
     $( "#audio-controls-toggle" ).click(function() {
         $( "#audio-controls" ).toggle();
     });
 
+    /* Adds the landingPage string literal as the full-screen-game container's html */
     $( "#full-screen-game" ).html(landingPage);
 
+    /* Toggles the Information modal */
     $( "#info-icon" ).click(function() {
         $( "#info-modal" ).modal();
     });
