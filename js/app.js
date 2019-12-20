59383b0f58ca4da10f51759a2127a0c03e539a1f
wondrousWebWorks <alberts.johann@gmail.com>
2019-12-20 13:13:42 +0000

    Adjusted code to show() or hide() game screens instead of loading html from JavaScript files.

--- js/app.js
+++ js/app.js
@@ -4,9 +4,6 @@ $(document).ready(function() {
         $( "#audio-controls" ).toggle();
     });
 
-    /* Adds the landingPage string literal as the full-screen-game container's html */
-    $( "#full-screen-game" ).html(landingPage);
-
     /* Toggles the Information modal */
     $( "#info-icon" ).click(function() {
         $( "#info-modal" ).modal();
@@ -15,7 +12,13 @@ $(document).ready(function() {
     /* Loads Select Level screen */
     $( "#play-icon" ).click(function() {
         $( "#full-screen-game-container-col" ).css("background", "url('https://res.cloudinary.com/wondrouswebworks/image/upload/v1576620172/realm-of-rantarctica/backgrounds/bg-4_ox6ev7.png')");
-        $( "#full-screen-game" ).html(levelSelectOne);
+        $( "#landing-page" ).hide();
+        $( "#level-select-page" ).show();
     });
 
+    $( "#target" ).click(function() {
+        $( "#full-screen-game-container-col" ).css("background", "url('https://res.cloudinary.com/wondrouswebworks/image/upload/v1576620176/realm-of-rantarctica/backgrounds/forest-bg-1_lqrdux.png')");
+        $( "#level-select-page" ).hide();
+        $( "#landing-page" ).show();
+    });
 });
