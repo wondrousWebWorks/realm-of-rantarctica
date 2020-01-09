56d26936f9a62bd7dc583db768a431c007dca9a2
Johann Alberts <alberts.johann@gmail.com>
2020-01-09 11:18:45 +0000

    Added a comment for playMusic().

--- js/app.js
+++ js/app.js
@@ -214,6 +214,7 @@ $(document).ready(function() {
         musicElement.volume = 0.5; 
     }
 
+    // Plays the currently loaded track on #music icon click
     function playMusic() {
         let musicElement = document.getElementById("music");
         musicElement.play();
