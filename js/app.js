ce48743396142b8824a1c543298e128a3523a971
Johann Alberts <alberts.johann@gmail.com>
2020-01-09 11:33:57 +0000

    Added a comment for .card mouseenter event listener.

--- js/app.js
+++ js/app.js
@@ -274,6 +274,7 @@ $(document).ready(function() {
         loadBattleScreen();    
     });
 
+    // Plays a sword swish sound on mouseenter of .card
     $( ".card ").mouseenter(function() {
         let soundFXElement = document.getElementById("sound-fx");
         soundFXElement.setAttribute("src", "assets/audio/zapsplat_leisure_toy_gun_shoot_puff_air_001_43142.mp3");
